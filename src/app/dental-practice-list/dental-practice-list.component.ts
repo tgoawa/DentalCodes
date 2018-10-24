a0895460047721d5f3f32a71838a8ac56df34300
Sledge <SledgeJ@schencksolutions.com>
2018-10-24 13:20:21 -0500

    create service to manage years

--- src/app/dental-practice-list/dental-practice-list.component.ts
+++ src/app/dental-practice-list/dental-practice-list.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-dental-practice-list',
   templateUrl: './dental-practice-list.component.html',
-  styleUrls: ['./dental-practice-list.component.css']
+  styleUrls: ['./dental-practice-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DentalPracticeListComponent implements OnInit {
+  @Input() dataSource: any[];
+  @Input() title: string;
 
   constructor() { }
 
---
+++ src/app/services/year.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YearService {
+  year: number[] = [2018];
+}
